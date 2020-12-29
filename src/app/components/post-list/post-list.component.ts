43336bcd28e7051eebb6c5554c9c49564a1211ec
Josh Levy <joshaaronlevy@gmail.com>
2020-12-28 20:04:10 -0700

    "Implemented Load More button functionality to post list"

--- src/app/components/post-list/post-list.component.ts
+++ src/app/components/post-list/post-list.component.ts
@@ -7,9 +7,11 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 })
 export class PostListComponent implements OnInit, OnDestroy {
   public posts: any[];
+  public newPosts: any[];
   page: number = 1;
   pageSize: number = 10;
   currentLength: number = 10;
+  moreAvailable: boolean;
 
   constructor() {}
 
@@ -23,12 +25,30 @@ export class PostListComponent implements OnInit, OnDestroy {
       page_size: this.pageSize
     }).then((res) => {
       this.posts = res.data.data;
-      console.log(this.page);
+      if (this.posts.length < 10) {
+        this.moreAvailable = false;
+      } else {
+        this.moreAvailable = true;
+      }
     });
   }
 
   loadMore() {
-
+    this.page = this.page + 1;
+    butterService.post.list({
+      page: this.page,
+      page_size: this.pageSize
+    }).then((res) => {
+      this.newPosts = res.data.data;
+      if (this.newPosts.length < 10) {
+        this.moreAvailable = false;
+      } else {
+        this.moreAvailable = true;
+      }
+      for (let i = 0; i < this.newPosts.length; i++) {
+        this.posts.push(this.newPosts[i]);
+      }
+    });
   }
 
   ngOnDestroy() {}
